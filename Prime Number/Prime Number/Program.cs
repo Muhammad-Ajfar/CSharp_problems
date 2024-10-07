de49d1e9cad16dec1c1d60b83a5da5d087b7df84
Muhammad-Ajfar <ajfarmuhammad@gmail.com>
2024-10-07 13:41:14 +0530

    fff

---
+++ Prime Number/Prime Number/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Prime_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            void checkPrime(int n)
+            {
+                if (n < 2) 
+                {
+                    Console.WriteLine($"{n} is not a Prime number");
+                    return;
+                }
+                for (int i = 2; i <= n / 2; i++)
+                {
+                    if(n%i == 0)
+                    {
+                        Console.WriteLine($"{n} is not a Prime number");
+                        return;
+                    }
+                }
+                Console.WriteLine($"{n} is a Prime number");
+            }
+            Console.WriteLine("Enter a number");
+            int num = Convert.ToInt32( Console.ReadLine() );
+            checkPrime(num);
+        }
+    }
+}
