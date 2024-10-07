6e0a637e8fbf02a8f00d7ebc4b91e7f009c089bc
Muhammad-Ajfar <ajfarmuhammad@gmail.com>
2024-10-07 12:28:37 +0530

    ff

---
+++ Diamond/Diamond/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Diamond
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the width of Diamond :");
+            int n =int.Parse(Console.ReadLine());
+            string str = "";
+            for (int i = 1; i < n * 2; i++)
+            {
+                if (i < n)
+                {
+                    str += new string(' ', n - i);
+                    str += new string('*', i * 2 - 1) + "\n";
+                }
+                else 
+                {
+                    str += new string(' ', i-n);
+                    str += new string('*', n*4-i*2-1) + "\n";
+                }
+            }
+            Console.WriteLine(str);
+        }
+    }
+}
